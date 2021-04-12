effff567dc37c833f672271ad17817dfd1d03cb2
Tobiloba Omitola <tboyomit@gmail.com>
2021-04-12 01:06:39 +0100

    Basic atm operation-First push to github

---
+++ atmmock.py
@@ -0,0 +1,45 @@
+AllowedUsers =  ['Seyi','Mike','Love']
+AllowedPassword = ['passwordSeyi','passwordMike','passwordLove']
+
+name = input('What is your name? \n')
+
+if(name in AllowedUsers):
+    password = input('What is your password? \n')
+    userId = AllowedUsers.index(name)
+
+    if(password == AllowedPassword[userId]):
+
+            import datetime as dt
+            current_datetime = dt.datetime.now()
+            print(current_datetime)
+
+            print('Welcome %s' %name)
+            print('These are the available options:')
+            print('1. Withdrawal')
+            print('2. Deposit')
+            print('3. Complaint')
+
+            SelectedOption = int(input('Please select an option: \n'))
+
+            if(SelectedOption == 1):
+                withdrawal_amount = int(input('How much would you like to withdraw? \n'))
+                print('Please take your cash')
+                print('Thank you for banking with us')
+
+            elif(SelectedOption == 2):
+                deposit_amount = int(input('How much would you like to deposit? \n'))
+                print('Your current balance is #%d' %deposit_amount)
+
+            elif(SelectedOption == 3):
+               complaint_type = input('What issue will you like to report? \n')
+               print('Thank you for contacting us. Your complaint has been received')
+               
+            else:
+                print('Invalid Option selected, please try again')
+                
+    else:
+            print('Password Incorrect, please try again')
+else:
+    print('Name not found, please try again')
+
+                 
